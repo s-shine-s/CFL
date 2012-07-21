53a01444d944375abf207abac19090a9401e6168
Shine <1612442429@qq.com>
2012-07-21 16:27:54 +0800

    [change] added package

---
+++ src/july21/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.shine;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String myWords = "Everything will be better!";
+		String firstWords = "Of course Hello World!!";
+		System.out.println("First Words:" + firstWords);
+		System.out.println("What I want to say is :" + myWords);
+
+	}
+
+}
