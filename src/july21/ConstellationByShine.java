25dcd43aab38ffc622ad36074718b676c235bb50
Shine <1612442429@qq.com>
2012-07-21 16:13:59 +0800

    Shine：星座运势

---
+++ src/july21/ConstellationByShine.java
@@ -0,0 +1,31 @@
+package com.shine;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ConstellationByShine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] consteList={"白羊座","金牛座","双子座","巨蟹座","狮子座","处女座",
+				"天秤座","天蝎座","射手座","摩羯座","水瓶座","双鱼座"};
+		String[] adviceList={"和朋友去吃饭！","约个会吧！","睡觉睡到自然醒！",
+				"不要对着电脑发呆！","去吃巧克力！","和家人聊天！",
+				"勇敢去表白吧！","学习该努力啦！","多喝水！",
+				"多睡觉！","准时吃饭！","玩会儿游戏吧！"};
+		Date date=new Date();
+		SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd");
+		String datestr=format.format(date);
+		System.out.println("时间："+ datestr);
+		System.out.println("-----------------星 座 运 势------------------");
+		for(int i=0;i<consteList.length;i++){
+			System.out.println(consteList[i] + "	" 
+					+ "运势: " + (int)( Math.random()*5+1) + "星" + " \t"
+					+ "建议: " + adviceList[(int)(Math.random()*adviceList.length)] );
+		}
+	}
+
+}
