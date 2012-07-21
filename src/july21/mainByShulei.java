d6e15e31c0cc983b086e617300cb0112bbfd5f0e
Qin Shulei <qsl19900126@gmail.com>
2012-07-21 12:09:58 +0800

    提交第一天的作业啦

---
+++ src/july21/mainByShulei.java
@@ -0,0 +1,15 @@
+package july21;
+
+public class mainByShulei {
+
+	/**
+	 * 第一题，控制台输出自己想说的话
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String content="Do the things that you want to do.Do the things which you will be proud of in all life";
+		System.out.println(content);
+	}
+	
+
+}
