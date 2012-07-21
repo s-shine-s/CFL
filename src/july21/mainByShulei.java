c4317f51dc8e603ee5e50e2619e3e6a844c53217
s-shine-s <1612442429@qq.com>
2012-07-21 14:01:01 +0800

    Update src/july21/mainByShulei.java

--- src/july21/mainByShulei.java
+++ src/july21/mainByShulei.java
@@ -3,11 +3,11 @@ package july21;
 public class mainByShulei {
 
 	/**
-	 * 第一题，控制台输出自己想说的话
-	 * @param args
+	 * 
+	 *  @param args
 	 */
 	public static void main(String[] args) {
-		String content="Do the things that you want to do.Do the things which you will be proud of in all life";
+		String content="Everything will be better!";
 		System.out.println(content);
 	}
 	
